5ae37ccda050b1b378a3f2d8e546f277d984a231
Ido Ben-Hur <idoybh2@gmail.com>
2022-01-20 08:10:07 +0530

    Atomichub: Add pref for qs brightness slider position [2/2]

--- src/com/palladium/atomichub/categories/frag_ui.java
+++ src/com/palladium/atomichub/categories/frag_ui.java
@@ -1,11 +1,22 @@
 package com.palladium.atomichub.categories;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.content.Context;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
 import com.android.settings.R;
@@ -16,9 +27,15 @@ import android.os.ServiceManager;
 import android.app.ActionBar;
 import com.palladium.atomichub.*;
 
+import com.palladium.support.preferences.SecureSettingMasterSwitchPreference;
+import com.palladium.support.preferences.SecureSettingSwitchPreference;
+
 public class frag_ui extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private IOverlayManager mOverlayService;
+    private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
+
+    private SecureSettingMasterSwitchPreference mBrightnessSlider;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +44,14 @@ public class frag_ui extends SettingsPreferenceFragment implements OnPreferenceC
         mOverlayService = IOverlayManager.Stub
                 .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
         //Feature Additon!
-
+        PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        mBrightnessSlider = (SecureSettingMasterSwitchPreference)
+                findPreference(BRIGHTNESS_SLIDER);
+        mBrightnessSlider.setOnPreferenceChangeListener(this);
+        enabled = Settings.Secure.getInt(resolver,
+                BRIGHTNESS_SLIDER, 1) == 1;
+        mBrightnessSlider.setChecked(enabled);
     }
     @Override
     public int getMetricsCategory() {
@@ -47,7 +71,12 @@ public class frag_ui extends SettingsPreferenceFragment implements OnPreferenceC
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final String key = preference.getKey();
-        return true;
+    if (preference == mBrightnessSlider) {
+            Boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    BRIGHTNESS_SLIDER, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
