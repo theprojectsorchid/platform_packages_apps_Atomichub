ef376f7267713ffdf81a4cdfe825de6f8f91b9e4
SKULSHADY <anushekprasal@gmail.com>
2022-01-20 09:19:19 +0530

    Atomichub: Notch friendly network traffic [2/2]

--- src/com/palladium/atomichub/categories/frag_ui.java
+++ src/com/palladium/atomichub/categories/frag_ui.java
@@ -30,15 +30,25 @@ import com.palladium.atomichub.*;
 import com.palladium.support.preferences.SecureSettingMasterSwitchPreference;
 import com.palladium.support.preferences.SecureSettingSwitchPreference;
 import com.palladium.support.preferences.SystemSettingMasterSwitchPreference;
+import com.palladium.support.preferences.SystemSettingSeekBarPreference;
+import com.palladium.support.preferences.SystemSettingSwitchPreference;
+
+import com.android.internal.util.palladium.PalladiumUtils;
 
 public class frag_ui extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private IOverlayManager mOverlayService;
     private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
     private static final String KEY_EDGE_LIGHTNING = "pulse_ambient_light";
+    private static final String KEY_NETWORK_TRAFFIC = "network_traffic_location";
+    private static final String KEY_NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
+    private static final String KEY_NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide_threshold";
 
     private SecureSettingMasterSwitchPreference mBrightnessSlider;
     private SystemSettingMasterSwitchPreference mEdgeLightning;
+    private ListPreference mNetworkTraffic;
+    private SystemSettingSwitchPreference mNetworkTrafficArrow;
+    private SystemSettingSeekBarPreference mNetworkTrafficAutohide;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,6 +73,26 @@ public class frag_ui extends SettingsPreferenceFragment implements OnPreferenceC
                 KEY_EDGE_LIGHTNING, 0, UserHandle.USER_CURRENT) == 1;
         mEdgeLightning.setChecked(enabled);
         mEdgeLightning.setOnPreferenceChangeListener(this);
+        
+        mNetworkTraffic = (ListPreference) findPreference(KEY_NETWORK_TRAFFIC);
+        int networkTraffic = Settings.System.getInt(resolver,
+        Settings.System.NETWORK_TRAFFIC_LOCATION, 0);
+        CharSequence[] NonNotchEntries = { getResources().getString(R.string.network_traffic_disabled),
+                getResources().getString(R.string.network_traffic_statusbar),
+                getResources().getString(R.string.network_traffic_qs_header) };
+        CharSequence[] NotchEntries = { getResources().getString(R.string.network_traffic_disabled),
+                getResources().getString(R.string.network_traffic_qs_header) };
+        CharSequence[] NonNotchValues = {"0", "1" , "2"};
+        CharSequence[] NotchValues = {"0", "2"};
+        mNetworkTraffic.setEntries(PalladiumUtils.hasNotch(getActivity()) ? NotchEntries : NonNotchEntries);
+        mNetworkTraffic.setEntryValues(PalladiumUtils.hasNotch(getActivity()) ? NotchValues : NonNotchValues);
+        mNetworkTraffic.setValue(String.valueOf(networkTraffic));
+        mNetworkTraffic.setSummary(mNetworkTraffic.getEntry());
+        mNetworkTraffic.setOnPreferenceChangeListener(this);
+
+        mNetworkTrafficArrow = (SystemSettingSwitchPreference) findPreference(KEY_NETWORK_TRAFFIC_ARROW);
+        mNetworkTrafficAutohide = (SystemSettingSeekBarPreference) findPreference(KEY_NETWORK_TRAFFIC_AUTOHIDE);
+        updateNetworkTrafficPrefs(networkTraffic);
     }
     @Override
     public int getMetricsCategory() {
@@ -92,7 +122,28 @@ public class frag_ui extends SettingsPreferenceFragment implements OnPreferenceC
             Settings.System.putIntForUser(resolver, KEY_EDGE_LIGHTNING,
                     value ? 1 : 0, UserHandle.USER_CURRENT);
             return true;
+        } else if (preference == mNetworkTraffic) {
+            int networkTraffic = Integer.valueOf((String) newValue);
+            int index = mNetworkTraffic.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_LOCATION, networkTraffic);
+            mNetworkTraffic.setSummary(mNetworkTraffic.getEntries()[index]);
+            updateNetworkTrafficPrefs(networkTraffic);
+            return true;
         }
         return false;
     }
+
+    private void updateNetworkTrafficPrefs(int networkTraffic) {
+        if (mNetworkTraffic != null) {
+            if (networkTraffic == 0) {
+                mNetworkTrafficArrow.setEnabled(false);
+                mNetworkTrafficAutohide.setEnabled(false);
+            } else {
+                mNetworkTrafficArrow.setEnabled(true);
+                mNetworkTrafficAutohide.setEnabled(true);
+            }
+        }
+    }
+
 }
